c21c0667283a0479fe48ba3e1b817e6672fa60d9
Back <joseozportillo@gmail.com>
2017-10-10 17:12:08 -0600

    agregando modulo alumnos

---
+++ addons/alumnos/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "alumnos",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Jose Zamora",
+    'website': "http://www.zamora.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mail'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'data/email_template_data.xml',
+        'views/views.xml',
+        'views/page_report.xml',
+        'views/report.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
